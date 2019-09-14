d00e6b2a42cc39293ece4f4e53cc3fbb53d963c5
erick.silva <ericksilva@localhost.localdomain>
2019-09-14 17:14:01 -0500

    v0.0.0-stateless-services.2 - long-lived firebase observables was converted to short-lived observables.

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { Observable } from 'rxjs';
-import { tap, map } from 'rxjs/operators';
+import { tap, map, first } from 'rxjs/operators';
 
 import { Course } from 'app/shared/model/course';
 import { Lesson } from 'app/shared/model/lesson';
@@ -27,10 +27,19 @@ export class CoursesService {
    */
   constructor(private db: AngularFireDatabase) { }
 
+  /**
+   * By default Angular Firebase dispatch long-lived observables it
+   * means that once subscribing to them a permanent connection is created.
+   * 
+   * In order to avoid long-lived observables we can take only the first 
+   * emitted value and disconnect from the source Observable. We can use first or 
+   * take(1) operators:
+   */
   findAllCourses(): Observable<Course []> {
     return this.db.list('courses')
       .valueChanges()
       .pipe(
+        first(),
         map(values => {
           console.log(`findAllCourses(): `, values);
           return values as unknown[] as Course [];
@@ -42,6 +51,7 @@ export class CoursesService {
     return this.db.list('lessons', ref => ref.orderByKey().limitToLast(10))
       .valueChanges()
       .pipe(
+        first(),
         map(values => {
           console.log(`findLatestLessons(): `, values);
           return values as unknown[] as Lesson [];
@@ -53,6 +63,7 @@ export class CoursesService {
     return this.db.list('courses', ref => ref.orderByChild('url').equalTo(courseUrl))
       .snapshotChanges()
       .pipe(
+        first(),
         map(data => {
           // console.log(`findCourseByUrl(courseUrl: string): `, data);
           // console.log(`data[0].payload.val(): `, data[0].payload.val());
@@ -69,6 +80,7 @@ export class CoursesService {
     return this.db.list('lessons', ref => ref.orderByChild('courseId').equalTo(courseId))
       .snapshotChanges()
       .pipe(
+        first(),
         map(data => {
           // console.log(`findLessonsByCourse(courseId: string): `, data);
           // console.log(`data[0].payload.val(): `, data[0].payload.val());
