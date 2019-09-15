ec1248fefffdefee7c9776f389a748307bdaac29
Erick Morales Silva <erick.silva@localhost.localdomain>
2019-09-15 16:26:45 -0500

    v0.0.0-observable-data-services.2 - Some comments were added in UserService class.

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -9,17 +9,39 @@ export const UNKNOWN_USER: User = {
   firstName: 'Unknown'
 };
 
+/**
+ * The main purpose of this kind of services is cache the data in memory.
+ * 
+ * They are highly independent of all application components 
+ * because they only retrieve the data, store it and let 
+ * consumers, via subscriptions, getting the data.
+ */
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
+  /**
+   * A Behaviour Subject is usually used to emit new values of data and store the last value. 
+   * Behaviour subject must be private.
+   */
   private userSubject: BehaviorSubject<User> = new BehaviorSubject(UNKNOWN_USER);
+
+  /**
+   * Expose an Observable in order to publish changes in data they hold.
+   */
   user$: Observable<User> = this.userSubject.asObservable();
 
+  /**
+   * They could have injectable instances of Stateless services.
+   */
   constructor(
     private httpClient: HttpClient) { }
 
+  /**
+   * Have a public API allowing to modify the data they are keeping.
+   */
   login(username: string, password: string): Observable<User> {
     let body = {
       email: username,
