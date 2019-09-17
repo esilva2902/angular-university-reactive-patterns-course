06dbb82fc4c4b07a624ede2828024f4c2b5d98d6
Erick Morales Silva <erick.silva@localhost.localdomain>
2019-09-17 12:56:19 -0500

    v0.0.0-buble-events.2 - Comments on NewsletterComponent were corrected.

--- src/app/newsletter/newsletter.component.ts
+++ src/app/newsletter/newsletter.component.ts
@@ -20,14 +20,13 @@ import { UserService } from 'app/services/user.service';
  * To display user's first name once the user has logged in, we have two options:
  * 
  * 1. We can inject an instance of ChangeDetectorRef. Once we have subscribed to 
- *    UserService.user$ observable in order to get the logged in user's first name
+ *    UserService.user$ observable in order to get the logged-in user's first name
  *    we have to call ChangeDetectorRef.markForCheck() method in order to Angular's Change
- *    Detection mechanism reflect the new value of NewsletterComponent.firstName property.
+ *    Detection mechanism reflects the new value of NewsletterComponent.firstName property.
  * 
  * 2. The second option is making NewsletterComponent.firstName property an Observable and
  *    then consume it from the template via the async pipe operator. When we consume observables
- *    from the template, Angular verifyies component's changing properties in despite of 
- *    Change Detection Strategy.
+ *    from the template, Angular verifies component's changing properties despite Change Detection Strategy.
  */
 
 export class NewsletterComponent implements OnInit {
